f4799b93715808c0de09c48052c58a139731932c
Ganesh Pawar <ws_htu1059@hoonartek03.net>
2023-08-10 16:39:05 +0530

    email service added

---
+++ src/main/java/com/example/registerapp/service/UserService.java
@@ -0,0 +1,69 @@
+package com.example.registerapp.service;
+
+import com.example.registerapp.dto.RegisterDto;
+import com.example.registerapp.entity.User;
+import com.example.registerapp.repository.UserRepository;
+import com.example.registerapp.util.EmailUtil;
+import com.example.registerapp.util.OtpUtil;
+import jakarta.mail.MessagingException;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private OtpUtil otpUtil;
+    @Autowired
+    private EmailUtil emailUtil;
+    @Autowired
+    private UserRepository userRepository;
+
+    public String register(RegisterDto registerDto) {
+        String otp = otpUtil.generateOtp();
+        try {
+            emailUtil.sendOtpEmail(registerDto.getEmail(), otp);
+        } catch (MessagingException e) {
+            throw new RuntimeException("Unable to send otp please try again");
+        }
+        User user = new User();
+        user.setName(registerDto.getName());
+        user.setEmail(registerDto.getEmail());
+        user.setPassword(registerDto.getPassword());
+        user.setOtp(otp);
+        user.setOtpGeneratedTime(LocalDateTime.now());
+        userRepository.save(user);
+        return "User registration successful";
+    }
+
+    public String verifyAccount(String email, String otp) {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("User not found with this email: " + email));
+        if (user.getOtp().equals(otp) && Duration.between(user.getOtpGeneratedTime(),
+                LocalDateTime.now()).getSeconds() < (1 * 300)) {
+            userRepository.save(user);
+            return "OTP verified Successfully";
+        }
+        return "Please regenerate otp and try again";
+    }
+
+    public String regenerateOtp(String email) {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("User not found with this email: " + email));
+        String otp = otpUtil.generateOtp();
+        try {
+            emailUtil.sendOtpEmail(email, otp);
+        } catch (MessagingException e) {
+            throw new RuntimeException("Unable to send otp please try again");
+        }
+        user.setOtp(otp);
+        user.setOtpGeneratedTime(LocalDateTime.now());
+        userRepository.save(user);
+        return "Email sent... please verify account within 1 minute";
+    }
+
+}
